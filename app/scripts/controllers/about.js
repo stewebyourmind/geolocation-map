c0b7ef643b17c02260de87f166654a3581851914
WebYourMind <stefano@webyourmind.com>
2016-02-04 17:14:32 +0100

    small changes

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -8,24 +8,15 @@
  * Controller of the geolocationMapApp
  */
 angular.module('geolocationMapApp')
-  .controller('AboutCtrl', function ($firebaseObject, FBURL, $log, $timeout) {
+  .controller('AboutCtrl', function ($log, firebasewrap) {
   	var AboutCtrl = this;
-    /*AboutCtrl.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-*/
 
-	var ref = new Firebase(FBURL);
 	// download the data into a local object
-	AboutCtrl.data = $firebaseObject(ref);
-
-
+	AboutCtrl.data = firebasewrap.getArray();
+	$log.debug("lunghezza: " , AboutCtrl.data.length);
+	$log.debug("tutto: ", AboutCtrl.data);
 	AboutCtrl.map = { center: { latitude: 45, longitude: -73 }, zoom: 15 };
 	
-	$timeout(function(){
-		$log.debug("Data length ["+AboutCtrl.data.length+"] - last item: ", AboutCtrl.data[AboutCtrl.data.length-1]);
-	}, 5000);
-	
+
+
   });
--- app/scripts/services/common.js
+++ app/scripts/services/common.js
@@ -1,13 +1,19 @@
 'use strict';
 
 angular.module('geolocationMapApp')
-.factory("firebaseref", ['$firebaseArray', 'FBURL', '$log',  function($firebaseArray, FBURL, $log) {
+.factory("firebasewrap", ['$firebaseArray', '$firebaseObject', 'FBURL', '$log',  function($firebaseArray, $firebaseObject, FBURL, $log) {
     var ref = new Firebase(FBURL);
 
     // this uses AngularFire to create the synchronized array
-    return $firebaseArray(ref);
+    //return $firebaseArray(ref);
+
+    return {
+    	getArray : function(){
+    		return $firebaseArray(ref);
+    	},
+    	getObject : function(){
+    		return $firebaseObject(ref);
+    	}    	
+    };
   }
 ]);
-
-
-$firebaseObject, FBURL, $log
